260b352552d3b613d191d896b471b0b63c46e90d
Andrei Beliankou <arbox@yandex.ru>
2014-11-18 18:45:42 +0100

    Added timing.

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 MALLET_DIR=mallet-2.0.7
 
+TIME_COMMAND="/usr/bin/time -a -v -o maarten_time_output.txt"
 INPUT_DIR=test_input
 OUTPUT_DIR=test_output
 
@@ -7,14 +8,14 @@ TOPIC_NUMBER=50
 WORD_NUMBER=20
 THRESHOLD=0.1
 
-${MALLET_DIR}/bin/mallet import-dir \
+${TIME_COMMAND} ${MALLET_DIR}/bin/mallet import-dir \
     --input ${INPUT_DIR} \
     --output ${INPUT_DIR}/topic-input.mallet \
     --keep-sequence --remove-stopwords TRUE\
     --stoplist-file ${MALLET_DIR}/stoplists/nl.txt \
     --skip-html TRUE
 
-${MALLET_DIR}/bin/mallet train-topics \
+${TIME_COMMAND} ${MALLET_DIR}/bin/mallet train-topics \
     --input ${INPUT_DIR}/topic-input.mallet \
     --num-topics ${TOPIC_NUMBER} \
     --output-state ${OUTPUT_DIR}/topic-state.gz \
